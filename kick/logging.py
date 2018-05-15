3c0beac02d4e0098b3ee3190a38860c3d986ad11
Alin Panaitiu <alin.p32@gmail.com>
2018-05-15 21:02:23 +0300

    Allow caller to get a basic logger

--- kick/logging.py
+++ kick/logging.py
@@ -2,23 +2,33 @@ import os
 import logging
 import pathlib
 
-import daiquiri
-
-LOGDIR = pathlib.Path.home() / '.logs'
+LOGDIR = pathlib.Path.home() / ".logs"
 
 
-def Logger(name=None, level=logging.INFO):
+def get_daiquiri_logger(name, level):
     if not LOGDIR.exists():
         LOGDIR.mkdir(parents=True)
+    import daiquiri
 
-    daiquiri.setup(outputs=(
-        daiquiri.output.STDERR,
-        daiquiri.output.File(directory=str(LOGDIR))
-    ), level=level)
+    daiquiri.setup(
+        outputs=(daiquiri.output.STDERR, daiquiri.output.File(directory=str(LOGDIR))),
+        level=level,
+    )
 
     logger = daiquiri.getLogger(name)
+    return logger
 
-    if os.getenv('DEBUG'):
+
+def Logger(name=None, level=logging.INFO, without_daiquiri=None):
+    daiquiri_env_key = "{}_NO_DAIQUIRI".format(name.upper())
+    without_daiquiri = without_daiquiri or os.getenv(daiquiri_env_key) in {"true", "1"}
+    if without_daiquiri:
+        logger = logging.getLogger(name)
+        logger.setLevel(level)
+    else:
+        logger = get_daiquiri_logger(name, level)
+
+    if os.getenv("DEBUG"):
         logger.setLevel(logging.DEBUG)
 
     return logger
