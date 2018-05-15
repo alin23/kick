f73e7aedf5a2c28e683f4e3e01d5bab94ee3231b
Alin Panaitiu <alin.p32@gmail.com>
2018-05-15 21:13:01 +0300

    Isolate env logic inside start

--- kick/__init__.py
+++ kick/__init__.py
@@ -1,13 +1,27 @@
-__version__ = '0.2.3'
+__version__ = "0.2.3"
+
+import os
 
 from .config import Config, update_config
 from .logging import Logger
 
 config = None
 logger = None
+APP_ENV_NAME = None
+TRUE_VALUES = {"true", "1"}
 
 
-def start(name, config_path=None, config_variant='config'):
-    global config, logger
+def start(name, config_path=None, config_variant="config", without_daiquiri=False):
+    global config, logger, APP_ENV_NAME
+
+    APP_ENV_NAME = name.upper().replace("-", "_").replace(" ", "_")
+    config_path = config_path or os.getenv("{}_CONFIG".format(APP_ENV_NAME), None)
+    config_variant = config_variant or os.getenv(
+        "{}_ENV".format(APP_ENV_NAME), "config"
+    )
+    without_daiquiri = without_daiquiri or os.getenv(
+        "{}_NO_DAIQUIRI".format(APP_ENV_NAME)
+    ) in TRUE_VALUES
+
     config = Config(name, config_path, variant=config_variant)
-    logger = Logger(name)
+    logger = Logger(name, without_daiquiri=without_daiquiri)
